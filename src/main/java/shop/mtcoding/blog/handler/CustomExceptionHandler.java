11062bbe5a37f4f3c1dac85de69db8d174154239
Hoseok Seong <shsshs0125@naver.com>
2023-02-11 15:44:44 +0900

    Feat: Exception 처리 완료

---
+++ src/main/java/shop/mtcoding/blog/handler/CustomExceptionHandler.java
@@ -0,0 +1,24 @@
+package shop.mtcoding.blog.handler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import shop.mtcoding.blog.dto.ResponseDto;
+import shop.mtcoding.blog.handler.exception.CustomApiException;
+import shop.mtcoding.blog.handler.exception.CustomException;
+import shop.mtcoding.blog.util.Script;
+
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<?> customException(CustomException e) {
+        return new ResponseEntity<>(Script.back(e.getMessage()), e.getStatus());
+    }
+
+    @ExceptionHandler(CustomApiException.class)
+    public ResponseEntity<?> customApiException(CustomApiException e) {
+        return new ResponseEntity<>(new ResponseDto<>(-1, e.getMessage(), null), e.getStatus());
+    }
+}
