a89752461da90cce4e90257c6935dd66c50ed8a0
Hoseok Seong <shsshs0125@naver.com>
2023-02-11 17:07:11 +0900

    Test: 로그인 Controller 테스트 코드 완료

--- src/test/java/shop/mtcoding/blog/controller/UserControllerTest.java
+++ src/test/java/shop/mtcoding/blog/controller/UserControllerTest.java
@@ -1,7 +1,10 @@
 package shop.mtcoding.blog.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import javax.servlet.http.HttpSession;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +15,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
+
+import shop.mtcoding.blog.model.User;
 
 /*
  * SpringBootTest는 통합테스트 (실제 환경과 동일하게 Bean이 생성됨)
@@ -35,6 +40,24 @@ public class UserControllerTest {
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE));
 
         // then
+        resultActions.andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    public void login_test() throws Exception {
+        // given
+        String requestBody = "username=ssar&password=1234";
+
+        // when
+        ResultActions resultActions = mvc.perform(post("/login").content(requestBody)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE));
+
+        HttpSession session = resultActions.andReturn().getRequest().getSession();
+        User principal = (User) session.getAttribute("principal");
+        // System.out.println(princiapl.getUsername()); 콘솔은 한 번만 직접 확인하면 된다.
+
+        // then
+        assertThat(principal.getUsername()).isEqualTo("ssar");
         resultActions.andExpect(status().is3xxRedirection());
     }
 }
