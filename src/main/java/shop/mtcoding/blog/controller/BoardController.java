b43323d92bfb7a1f4e56b96ecfc2025277bb2d90
Hoseok Seong <shsshs0125@naver.com>
2023-02-11 17:45:03 +0900

    Feat: 리뷰 작성 Controller, Service 완료

--- src/main/java/shop/mtcoding/blog/controller/BoardController.java
+++ src/main/java/shop/mtcoding/blog/controller/BoardController.java
@@ -1,10 +1,30 @@
 package shop.mtcoding.blog.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import shop.mtcoding.blog.dto.ResponseDto;
+import shop.mtcoding.blog.dto.board.BoardReqDto.BoardSaveReqDto;
+import shop.mtcoding.blog.handler.exception.CustomApiException;
+import shop.mtcoding.blog.model.User;
+import shop.mtcoding.blog.service.BoardService;
 
 @Controller
 public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
+
+    @Autowired
+    private HttpSession session;
 
     @GetMapping({ "/", "/board" })
     public String main() {
@@ -19,6 +39,31 @@ public class BoardController {
     @GetMapping("/board/saveForm")
     public String saveForm() {
         return "/board/saveForm";
+    }
+
+    @PostMapping("/board")
+    public @ResponseBody ResponseEntity<?> save(@RequestBody BoardSaveReqDto boardSaveReqDto) {
+        User principal = (User) session.getAttribute("principal");
+        if (principal == null) {
+            throw new CustomApiException("인증이 실패했습니다", HttpStatus.UNAUTHORIZED);
+        }
+        if (boardSaveReqDto.getTitle() == null ||
+                boardSaveReqDto.getTitle().isEmpty()) {
+            throw new CustomApiException("제목을 작성해주세요");
+        }
+
+        if (boardSaveReqDto.getTitle().length() > 100) {
+            throw new CustomApiException("제목의 길이는 100자 이하까지 가능합니다");
+        }
+
+        if (boardSaveReqDto.getContent() == null ||
+                boardSaveReqDto.getContent().isEmpty()) {
+            throw new CustomApiException("글 내용을 작성해주세요");
+        }
+
+        boardService.글쓰기(boardSaveReqDto, principal.getId());
+
+        return new ResponseEntity<>(new ResponseDto<>(1, "글쓰기 성공", null), HttpStatus.CREATED);
     }
 
     @GetMapping("/board/updateForm")
---
+++ src/main/java/shop/mtcoding/blog/service/BoardService.java
@@ -0,0 +1,28 @@
+package shop.mtcoding.blog.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import shop.mtcoding.blog.dto.board.BoardReqDto.BoardSaveReqDto;
+import shop.mtcoding.blog.handler.exception.CustomApiException;
+import shop.mtcoding.blog.model.BoardRepository;
+
+@Transactional(readOnly = true)
+@Service
+public class BoardService {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    // where 절에 걸리는 파라미터를 앞에 받기.
+    @Transactional
+    public void 글쓰기(BoardSaveReqDto boardSaveReqDto, int userId) {
+        int result = boardRepository.insert(userId, boardSaveReqDto.getTitle(), boardSaveReqDto.getContent(), null);
+        if (result != 1) {
+            throw new CustomApiException("글 작성이 실패하였습니다", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+}
