468dd125d2fc856cbed2e8a000345e08072704ae
Hoseok Seong <shsshs0125@naver.com>
2023-02-11 18:32:36 +0900

    Feat: 리뷰 상세 보기 Controller 완료

--- src/main/java/shop/mtcoding/blog/controller/BoardController.java
+++ src/main/java/shop/mtcoding/blog/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -70,6 +71,12 @@ public class BoardController {
         boardService.글쓰기(boardSaveReqDto, principal.getId());
 
         return new ResponseEntity<>(new ResponseDto<>(1, "글쓰기 성공", null), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/board/{id}")
+    public String detail(@PathVariable int id, Model model) {
+        model.addAttribute("boardDto", boardRepository.findByIdWithUser(id));
+        return "/board/detail";
     }
 
     @GetMapping("/board/updateForm")
